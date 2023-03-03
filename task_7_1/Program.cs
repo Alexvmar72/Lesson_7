25f6641c01a325f11936e6aa18729725645269a9
Alex Mark <alexvmar@yandex.ru>
2023-03-03 20:17:57 +0300

    change files homework7

---
+++ task_7_1/Program.cs
@@ -0,0 +1,58 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9        */
+
+using System;
+using static System.Console;
+
+Clear();
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{Math.Round(inArray[i, j], 1)} ");
+        }
+        WriteLine();
+    }
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+double PromptDouble(string message)
+{
+    Console.Write(message);
+    double result = double.Parse(Console.ReadLine()!);
+    return result;
+}
+
+int m = Prompt("Введите размер массива по строкам: ");
+int n = Prompt("Введите размер массива по столбцам: ");
+double minValue = PromptDouble("Введите минимальное значение массива: ");
+double maxValue = PromptDouble("Введите максимальное значение массива: ");
+
+PrintArray(GetArray(m, n, minValue, maxValue));
---
+++ task_7_2/Program.cs
@@ -0,0 +1,48 @@
+﻿/*      Задача 50. Напишите программу, которая на вход принимает позиции 
+элемента в двумерном массиве, и возвращает значение этого элемента 
+или же указание, что такого элемента нет.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет     */
+
+using System;
+using static System.Console;
+
+Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int result = int.Parse(Console.ReadLine()!);
+    return result;
+}
+
+string SearchNumArray(int m, int n, int[,] Array)
+{
+    string result;
+    if (m > Array.GetLength(0) || n > Array.GetLength(1))
+    {
+        result = "такого числа в массиве нет";
+    }
+    else
+    {
+        result = $"{Array[m, n]}";
+    }
+    return result;
+}
+
+int[,] Array = new int[,]
+{
+{1, 4, 7, 2},
+{5, 9, 2, 3},
+{8, 4, 2, 4},
+{7, 5, 23, 53453},
+};
+
+int m = Prompt("Введите номер строки: ");
+int n = Prompt("Введите номер столбца: ");
+WriteLine($"{m}{n} -> {SearchNumArray(m, n, Array)}");
